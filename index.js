966d1027f8ac5af4b01ec1da1b5bbc29a75eb292
Roman <komanukroman717@gmail.com>
2022-05-19 15:30:21 +0200

    'операторы'

--- index.js
+++ index.js
@@ -19,3 +19,56 @@
 // personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB);
+
+// const hamburger = 5;
+// const fries = 1;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!')
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+// console.log(hamburger === 3 && cola && fries);
+
+// if (hamburger === 3 && cola && fries) {
+//     console.log('Все сыты');
+// } else {
+//     console.log("Мы уходим!");
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 1;
+// const naggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && naggets) {
+//     console.log('Все Довольны');
+// } else {
+//     console.log("Мы уходим!");
+// }
+
+// console.log(fries === 3 && (naggets === 2 || hamburger === 3) && cola === 5);
+
+
+let hamburger;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+
+
+// let jhonReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(jhonReport || alexReport || samReport || mariaReport)
+
+// if (jhonReport || alexReport || samReport || mariaReport) {
+//     console.log('Отчеты есть');
+// } else {
+//     console.log('Отчетов нет');
+// }
