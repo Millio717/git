8faf132ca9d34f2168009cd7d97c7e4265cdc571
Roman <komanukroman717@gmail.com>
2022-05-31 18:59:55 +0200

    'tasks'

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-let words = 'aaa sss ddd';
-words = words.split(' ');
+// Вывести числа от 4 до 400 на экран
+let out = document.getElementById('out');
+let tmp = '';
+// for (let i = 4; i <= 400; i++) {
+//     tmp += i + ' ';
+// }
+// out.innerHTML = tmp;
 
-for(let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-}
-console.log(words.join(' '));
+
+// Вывести числа в последовательности 4 7 10 13 с помощью цикла
+// for (let i = 4; i <= 13; i+=3) {
+//     tmp += i + ' ';
+// }
+// out.innerHTML = tmp;
+
+// Вывести числа 654 653 652 до нуля
+// for (let i = 654; i >= 0 && i <= 654; i--) {
+//         tmp += i + ' ';
+// }
+
+// let i = 654;
+// while (i >= 0 && i <= 654) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// Вывести все годы с 1983 до 2017
+// for (let i = 1983; i <= 2017; i++) {
+//     tmp += i + ' ';
+// }
+// out.innerHTML = tmp;
+
+// let i = 1983;
+// while (i >= 1983 && i <= 2017) {
+//     tmp += i + ' ';
+//     i++;
+// }
+// out.innerHTML = tmp;
+
+// Вывести числа -4 -2 0 2 4 6 _ 100
+// for (let i = -4; i <= 100; i+=2) {
+//     tmp += i + ' ';
+// }
+// out.innerHTML = tmp;
+
+// let i = -4;
+// while (i >= -4 && i <=100) {
+//     tmp += i + ' ';
+//     i += 2;
+// }
+// out.innerHTML = tmp;
+
+// Таблица умножения на 7
+// let seven = '';
+// for (let i = 1; i <= 9; i++) {
+//     tmp += +7+ ' * ' +i+ ' = ' +7 * i + '\n';
+// }
+// out.innerHTML = tmp;
+
+// Выведите с помощью цикла коды спецсимволов от 1000 до 2000. Напомню что спецсимвол получается комбинацией &#число; Например &#1222
+
+// for (let i =7000; i <= 10000; i++) {
+//     tmp += '&#' + i + ' ss';
+// }
+// out.innerHTML = tmp;
